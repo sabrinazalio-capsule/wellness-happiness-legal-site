d223b1604295b125b866e8859415b77142c69dd9
Lucas Mansilla <lucas_e_93@hotmail.com>
2026-02-09 17:36:51 -0300

    Update BrowserRouter in main.jsx to include basename for Vite deployment compatibility

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ if (redirect && redirect !== location.pathname) {
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter  basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
